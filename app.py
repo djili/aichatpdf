d51281a43133b75137b55e96b0a36c0887b5f19d
Abdou Khadre DIOP <diopous1@gmail.com>
2025-06-26 13:46:12 +0000

    add streamlit

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello word")
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Rag with PDF", page_icon=":books:")
+    st.title("Discutez avec votre PDF")
+    st.text_input("input")
+    with st.sidebar:
+        st.subheader("subheader")
+        st.file_uploader("file uploader")
+        st.button("button")
+
+if __name__ == "__main__":
+    main()
